1961995ec6fead7e789ccb1066db07628cc64f77
victorgileau <vgileau@hotmail.com>
2024-10-17 23:35:09 -0400

    ajout_btn_toggle-darkBlue

--- js/script.js
+++ js/script.js
@@ -1,14 +1,76 @@
 /*-- Code Track mouse --*/
 
 const pointer = document.querySelector('.pointer');
-
-document.body.onpointermove = event => {
-    const { clientX, clientY } = event;
-
-    pointer.animate({
-        left: `${clientX}px`,
-        top: `${clientY}px`
+if (pointer != null) {
+    document.body.onpointermove = event => {
+        const { clientX, clientY } = event;
     
-    }, {duration: 500, fill: "forwards"})
-
+        pointer.animate({
+            left: `${clientX}px`,
+            top: `${clientY}px`
+        
+        }, {duration: 500, fill: "forwards"})
+    
+    }
 }
+
+const main = document.querySelector('main');
+const containerNav = document.querySelector('.bgNav');
+const footer = document.querySelector('footer');
+const modalConteneur = document.querySelector('.modalConteneur');
+
+
+const checkbox = document.querySelector('.checkbox');
+const slider = document.querySelector('.toggle__slider');
+slider.style.alignSelf = 'start';
+
+if (sessionStorage.getItem('ifCheckedTrue') != null || sessionStorage.getItem('ifCheckedFalse')) {
+    if (sessionStorage.getItem('ifCheckedTrue')) {
+        checkbox.checked = true;
+        slider.style.alignSelf = 'end';
+        main.classList.add('darkBlue');
+        containerNav.classList.add('darkBlue');
+        footer.classList.add('darkBlue');
+        if (modalConteneur != null) {
+            modalConteneur.classList.add('darkBlue');
+        }
+    } else {
+        checkbox.checked = false;
+        slider.style.alignSelf = 'start';
+        main.classList.remove('darkBlue');
+        containerNav.classList.remove('darkBlue');
+        footer.classList.remove('darkBlue');
+        if (modalConteneur != null) {
+            modalConteneur.classList.remove('darkBlue');
+        }
+    }
+    
+}
+
+function toggleCheckBox() {
+    console.log('hello');
+    if (checkbox.checked == true) {
+        sessionStorage.removeItem('ifCheckedFalse');
+        sessionStorage.setItem('ifCheckedTrue', 'true');
+        slider.style.alignSelf = 'end';
+        main.classList.add('darkBlue');
+        containerNav.classList.add('darkBlue');
+        footer.classList.add('darkBlue');
+        if (modalConteneur != null) {
+            modalConteneur.classList.add('darkBlue');
+        }
+    } else {
+        sessionStorage.removeItem('ifCheckedTrue');
+        sessionStorage.setItem('ifCheckedFalse', 'false');
+        slider.style.alignSelf = 'start';
+        main.classList.remove('darkBlue');
+        containerNav.classList.remove('darkBlue');
+        footer.classList.remove('darkBlue');
+        if (modalConteneur != null) {
+            modalConteneur.classList.remove('darkBlue');
+        }
+        
+    }
+}
+
+checkbox.addEventListener('click', toggleCheckBox);
